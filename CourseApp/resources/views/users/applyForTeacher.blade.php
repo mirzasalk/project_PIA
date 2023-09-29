1acc430ab80ad01f149820284022ca459ffae4b1
Mirza Salkovic <salkovic.mirza@gmail.com>
2023-09-29 06:36:48 +0200

    add applyForTeacher page

--- CourseApp/resources/views/users/applyForTeacher.blade.php
+++ CourseApp/resources/views/users/applyForTeacher.blade.php
@@ -1,5 +1,20 @@
+@php
+    $ime = auth()->user()->name;
+    $email = auth()->user()->email;
+    $id = auth()->user()->id;
+  
+@endphp
 <x-layout>
     <div>
-        <h1>Apliciraj za učitelja</h1>
+        <h1>Vase informacije</h1>
+        <h5><strong>Ime:</strong>{{ $ime}}</h5>
+        <h5><strong>email:</strong>{{ $email}}</h5>
+        <h5><strong>id:</strong>{{ $id}}</h5>
+        <form method="POST" action="/requestSend">
+            @csrf
+            <button>Pošalji zahtev</button>
+        </form>
+        
+
     </div>
 </x-layout>
